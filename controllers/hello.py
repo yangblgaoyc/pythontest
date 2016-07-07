70f15ad4625e6f1c812253b2d9651c96e27797c9
杨博伦 <yangbolun@yangbolundeMac-mini.local>
2016-07-07 12:05:20 +0800

    python第一个增删改查

--- controllers/hello.py
+++ controllers/hello.py
@@ -7,9 +7,11 @@ from application import db
 from models.model import User
 from flask import jsonify
 
+
 @bp.route('/hello/', methods=['GET', 'POST'])
 @bp.route('/hello/<name>', methods=['GET', 'POST'])
 def hello(name=None):
+
     if request.method == 'GET':
         users = get_all()
         return render_template('hello.html', name=name, users=users)
@@ -20,15 +22,25 @@ def hello(name=None):
     add(username, email)
     return redirect(url_for('.hello'))
 
+
+def add(username, email):
+    user = User(username=username, email=email)
+
+    db.session.add(user)
+    db.session.commit()
+
+
 @bp.route('/hello/<int:user_id>/delete')
 def delete_user(user_id):
     delete(user_id)
     return jsonify_with_data((200, 'OK'))
 
+
 def delete(user_id):
     user = User.query.get(user_id)
     db.session.delete(user)
     db.session.commit()
+
 
 @bp.route('/hello/<int:user_id>/edit', methods=['GET', 'POST'])
 def edit_user(user_id):
@@ -38,12 +50,14 @@ def edit_user(user_id):
 
     return jsonify_with_data((200, 'OK'))
 
+
 def edit(user_id, username, email):
     user_page = User.query.get(user_id)
     user_page.username = username
     user_page.email = email
     db.session.add(user_page)
     db.session.commit()
+
 
 def get_all(type=None):
     stmt = User.query.filter_by()
@@ -54,11 +68,6 @@ def get_all(type=None):
     users = stmt.order_by(User.id.asc()).all()
     return [user.to_dict() for user in users]
 
-def add(username, email):
-    user = User(username=username, email=email)
-
-    db.session.add(user)
-    db.session.commit()
 
 def jsonify_with_data(err, **kwargs):
     resp = {
